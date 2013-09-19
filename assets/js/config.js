c37577558591cc4ec888276160e2e541c5d8ce74
Eric Owle <eric.owle@gmail.com>
2013-09-19 17:48:30 -0400

    Game day template nearly done.

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,10 +2,20 @@
  * Configuration variables.
  */
 define([], function(){
+  if(window.location.href.indexOf('localhost') !== -1)
+  {
+    return {
+      'URLs': {
+        'Base':'http://localhost/',
+        'API': 'http://localhost/api/'
+      }
+    }
+  }
+
   return {
     'URLs': {
-      'Base': 'http://localhost/',
-      'API':  'http://localhost/api/'
+      'Base': 'http://notsoexpert-dev.elasticbeanstalk.com/',
+      'API':  'http://notsoexpert-dev.elasticbeanstalk.com/api/'
     }
   }
 });
--- assets/js/views/GameDayView.js
+++ assets/js/views/GameDayView.js
@@ -2,8 +2,9 @@ define(['backbone',
         'moment',
         'js/models/GameDayModel',
         'text!templates/GameDayTemplate.html',
-        'text!templates/MemberRowTemplate.html'],
-       function(Backbone, moment, GameDayModel, GameDayTemplate, MemberRowTemplate) {
+        'text!templates/MemberRowTemplate.html',
+        'text!templates/MemberPicksTemplate.html'],
+       function(Backbone, moment, GameDayModel, GameDayTemplate, MemberRowTemplate, MemberPicksTemplate) {
   return Backbone.View.extend({
     model: null,
 
@@ -12,6 +13,47 @@ define(['backbone',
       this.model.on('sync', this.render, this);
       this.model.fetch();
     },
+
+    renderPicks: function(schedule, members)
+    {
+      var tmpObj = {},
+          cur_pick = '',
+          rendered_template,
+          toRender = [],
+          directives = {
+            'game_time': {
+              text: function() {
+                return moment(this.game_time).format('dddd h:mm');
+              }
+            }
+          };
+
+      $(schedule).each(function(k, game){
+        tmpObj = game;
+        tmpObj.member_picks = [];
+        $.each(members, function(member_id, member_data) {
+          if(member_data.picks && member_data.picks.hasOwnProperty(game.game_id)) {
+            if(member_data.picks[game.game_id].pick === true) {
+              cur_pick = 'MADE'
+            }
+            else {
+              cur_pick = member_data.picks[game.game_id].pick;
+            }
+          }
+          else {
+            cur_pick = "NOT";
+          }
+
+          tmpObj.member_picks.push({'member_pick': cur_pick});
+        });
+
+        toRender.push(tmpObj);
+      });
+
+      rendered_template = $(MemberPicksTemplate).render(toRender, directives);
+      return rendered_template;
+    },
+
     renderMembers: function(members) {
       var members_array = [],
           directives = {
@@ -35,8 +77,12 @@ define(['backbone',
 
     render: function() {
       var member_template = this.renderMembers(this.model.attributes.members);
+      member_template.prepend('<div class="schedule-box" style="visibility: hidden; margin-top: 24px;"></div>');
+      var picks_template = this.renderPicks(this.model.attributes.schedule, this.model.attributes.members);
       var rendered_template = $(GameDayTemplate).render(this.model.attributes);
-      rendered_template.find('.member-row').append(member_template);
+
+      rendered_template.find('.member-row').append(member_template).end()
+                       .find('.member-picks-row').append(picks_template);
       this.$el.append(rendered_template).find('[data-toggle="tooltip"]').tooltip();
     }
   });
